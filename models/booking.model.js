e4d2f48b9602f2ed05acca1b2f4ae960a07baa11
Chacole <164119802+Chacole@users.noreply.github.com>
2024-12-20 01:13:16 +0700

    Add files via upload

---
+++ models/booking.model.js
@@ -0,0 +1,59 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const Hotel = require('./hotel.model');
+const Room = require('./room.model');
+
+const Booking = sequelize.define('Booking', {
+    booking_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    guest_name: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    hotel_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Hotel,
+            key: 'hotel_id'
+        },
+        onDelete: 'CASCADE',  // Xử lý khi hotel bị xóa
+        onUpdate: 'CASCADE'   // Xử lý khi hotel bị cập nhật
+    },
+    room_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Room,
+            key: 'room_id'
+        },
+        onDelete: 'CASCADE',  // Xử lý khi room bị xóa
+        onUpdate: 'CASCADE'   // Xử lý khi room bị cập nhật
+    },
+    check_in_date: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    check_out_date: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    total_amount: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: true
+    },
+    status: {
+        type: DataTypes.ENUM('confirm', 'reject'),
+        allowNull: true
+    }
+}, {
+    tableName: 'bookings',
+    timestamps: false
+});
+
+// Quan hệ với Hotel và Room
+Booking.belongsTo(Hotel, { foreignKey: 'hotel_id' });
+Booking.belongsTo(Room, { foreignKey: 'room_id' });
+
+module.exports = Booking;
